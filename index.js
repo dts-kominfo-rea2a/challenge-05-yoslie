f844ffb6677e450acc35fb8ba5755a722ad25f14
Yoslie <yoslie.tan@gmail.com>
2022-06-04 23:16:12 +0700

    feat: update function sort data asc and desc

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, sortByFunction) => {
+
+  const sortedData = sortByFunction(data)
+  const numberedData = []
+
+  for (let i = 0; i < sortedData.length; i++) {
+    numberedData.push(`${(i+1)}. ${sortedData[i]}`)
+  }
+
+  return numberedData
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+  const sortedDataAsc = data.sort()
+  return sortedDataAsc
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+  const sortedDataDesc = data.sort().reverse()
+  return sortedDataDesc
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
